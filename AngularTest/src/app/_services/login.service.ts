123fe571394848fd1e0f1190c1671d6b2dcd0823
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2022-01-04 03:14:20 +0800

    Fixed login

--- AngularTest/src/app/_services/login.service.ts
+++ AngularTest/src/app/_services/login.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const APILoginURL = 'http://localhost:83/api/login/loginDB';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public login (name: string, pass: string) : Observable<any> { 
+    
+    return this.httpClient.post(APILoginURL, { userName: name, password: pass}, {withCredentials:true})
+  }
+  
 }
--- AngularTest/src/app/login/login.component.ts
+++ AngularTest/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../_services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  name:string='';
+  password:string='';
+  message:string="";
 
-  constructor() { }
+
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  public login():void{
+    this.loginService.login(this.name, this.password).subscribe({
+      next: (res:any) => {
+        if(res.value.status) {
+
+          this.router.navigate(['home'])
+          this.message = res.value.message;
+        }
+        else {
+          this.message = res.value.message;
+        }
+      },
+      error: (error:any) => {
+        this.message = "Unknown Error occured, please try again"  
+        this.message = error;
+      }
+    
+    });
+  }
+  
 
 }
